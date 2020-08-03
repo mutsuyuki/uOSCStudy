e6eae544f93ceb73f9ed60082e744db824a382c3
mutsuyuki <muttuwo.mob@gmail.com>
2020-08-03 22:19:09 +0900

    Initial commit

---
+++ Assets/RecieveTest.cs
@@ -0,0 +1,21 @@
+﻿using uOSC;
+using UnityEngine;
+
+[RequireComponent(typeof(uOscServer))]
+public class RecieveTest : MonoBehaviour {
+    void Start() {
+        var server = GetComponent<uOscServer>();
+
+        server.onDataReceived.AddListener(message => {
+            Vector3 pos = transform.position;
+            pos.x = (float) message.values[0];
+            pos.y = (float) message.values[1];
+            transform.position = pos;
+
+
+            Debug.Log(
+                message.address + ":"
+            );
+        });
+    }
+}
---
+++ Assets/SendTest.cs
@@ -0,0 +1,16 @@
+﻿using uOSC;
+using UnityEngine;
+
+[RequireComponent(typeof(uOscClient))]
+public class SendTest : MonoBehaviour {
+    void Update() {
+        var client = GetComponent<uOscClient>();
+
+        object[] data = new object[2] {
+            Mathf.Sin((float) Time.frameCount / 10f),
+            Mathf.Sin((float) Time.frameCount / 5f) * 2.0f
+        };
+
+        client.Send("/testsend", data);
+    }
+}
